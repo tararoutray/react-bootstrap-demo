36c87e8397a883bd645efe3581a729951b361274
Tara Prasad Routray <tararoutray@gmail.com>
2022-10-05 09:39:52 +0530

    Added button component.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ButtonDemo from './ButtonDemo';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<React.Fragment>
+			<div className='container py-5'>
+				<h1>Button Component</h1>
+				<div className='mt-4'>
+					<ButtonDemo></ButtonDemo>
+				</div>
+			</div>
+		</React.Fragment>
+	);
 }
 
 export default App;
---
+++ src/ButtonDemo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+const ButtonDemo = () => {
+    return (
+        <React.Fragment>
+            <Button variant="primary">Primary</Button>{' '}
+            <Button variant="secondary">Secondary</Button>{' '}
+            <Button variant="success">Success</Button>{' '}
+            <Button variant="warning">Warning</Button>{' '}
+            <Button variant="danger">Danger</Button>{' '}
+            <Button variant="info">Info</Button>{' '}
+            <Button variant="light">Light</Button>{' '}
+        </React.Fragment>
+    );
+}
+
+export default ButtonDemo;
