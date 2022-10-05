4aa47b97aafa5f9c5f9bf838196d257cabdff88a
Tara Prasad Routray <tararoutray@gmail.com>
2022-10-05 23:26:44 +0530

    Added alert component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import React from 'react';
 import ButtonDemo from './ButtonDemo';
+import AlertDemo from './AlertDemo';
+import SpinnerDemo from './SpinnerDemo';
 
 function App() {
 	return (
@@ -9,6 +11,16 @@ function App() {
 				<h1>Button Component</h1>
 				<div className='mt-4'>
 					<ButtonDemo></ButtonDemo>
+				</div>
+				<div className='my-5'></div>
+				<h1>Alert Component</h1>
+				<div className='mt-4'>
+					<AlertDemo></AlertDemo>
+				</div>
+				<div className='my-5'></div>
+				<h1>Spinner Component</h1>
+				<div className='mt-4'>
+					<SpinnerDemo></SpinnerDemo>
 				</div>
 			</div>
 		</React.Fragment>
